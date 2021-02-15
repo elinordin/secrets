e3b599c0c48886f28a06ec69d8758f9bf86e30fc
elinordin <elin.evelina@live.se>
2021-02-15 21:03:44 +0100

    level seven authentication - adding oauth authentication through google to outsource the handeling of user-password data from our own database, have access to our users profile information and to allow faster sign-in for the user

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const mongoose = require('mongoose')
 const session = require('express-session')
 const passport = require('passport')
 const passportLocalMongoose = require('passport-local-mongoose')
-
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate')
 
 const app = express()
 
@@ -28,22 +29,69 @@ mongoose.set('useCreateIndex', true)
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String
 })
 
 userSchema.plugin(passportLocalMongoose)
+userSchema.plugin(findOrCreate)
 
 const User = new mongoose.model('user', userSchema)
 
 passport.use(User.createStrategy())
-passport.serializeUser(User.serializeUser())
-passport.deserializeUser(User.deserializeUser())
+
+passport.serializeUser(function(user, done){
+    done(null, user.id)
+})
+
+passport.deserializeUser(function(id, done){
+    User.findById(id, function(err,user) {
+        done(err, user)
+    })
+})
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: 'http://localhost:3000/auth/google/secrets',
+    userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo'
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile)
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+
 
 app.get('/', function(req, res){
     res.render('home')
 });
 
+
+
+app.route('/auth/google')
+
+  .get(passport.authenticate('google', {
+      scope: ['profile']
+  }));
+
+
+
+app.route('/auth/google/secrets')
+
+    .get(passport.authenticate('google', {
+        failureRedirect: ['/login']}),
+        function(req, res) {
+            res.redirect('/secrets')
+    });
+
+
+
 app.route('/login')
+
     .get(function(req, res){
         res.render('login')
     })
@@ -66,6 +114,7 @@ app.route('/login')
     });
 
 
+
 app.route('/register')
 
     .get(function(req, res){
@@ -85,6 +134,7 @@ app.route('/register')
             }
         })
     });
+
 
 
 app.route('/secrets')
