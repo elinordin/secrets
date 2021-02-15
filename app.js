5c49ad75f9f16c6e545bcaee8ba23135ef245d1f
elinordin <elin.evelina@live.se>
2021-02-15 21:27:16 +0100

    applying functionality to submit a secret and display them on the secrets page using ejs

--- app.js
+++ app.js
@@ -30,7 +30,8 @@ mongoose.set('useCreateIndex', true)
 const userSchema = new mongoose.Schema({
     email: String,
     password: String,
-    googleId: String
+    googleId: String,
+    secret: String
 })
 
 userSchema.plugin(passportLocalMongoose)
@@ -57,12 +58,12 @@ passport.use(new GoogleStrategy({
     userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo'
   },
   function(accessToken, refreshToken, profile, cb) {
-    console.log(profile)
     User.findOrCreate({ googleId: profile.id }, function (err, user) {
       return cb(err, user);
     });
   }
 ));
+
 
 
 
@@ -140,11 +141,44 @@ app.route('/register')
 app.route('/secrets')
 
     .get(function(req, res){
+        User.find({'secret': {$ne:null}}, function (err, foundUsers){
+            if (err){
+                console.log(err)
+            } else {
+                if (foundUsers) {
+                    res.render('secrets', {usersWithSecrets: foundUsers})
+                }
+            }
+        })
+    });
+
+
+
+app.route('/submit')
+
+    .get(function(req, res){
         if (req.isAuthenticated()){
-            res.render('secrets')
+            res.render('submit')
         } else {
-            res.redirect('/')
+            res.redirect('/login')
         }
+    })
+
+    .post(function(req, res){
+        const submittedSecret = req.body.secret
+        
+        User.findById(req.user.id, function(err, foundUser){
+            if (err) {
+                console.log(err)
+            } else {
+                if (foundUser) {
+                    foundUser.secret = submittedSecret
+                    foundUser.save(function(){
+                        res.redirect('/secrets')
+                    })
+                }
+            }
+        })
     });
 
 
