7a90d08e468129560f6d1ba57f1fdcea4feb7a53
elinordin <elin.evelina@live.se>
2021-02-15 14:57:59 +0100

    level four authentication - password is now hashed instead of encrypted, which means it does not use a key and is therefore almost impossible to decrypt

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const ejs = require('ejs')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
-const encrypt = require('mongoose-encryption')
+const md5 = require('md5')
 
 const app = express()
 
@@ -17,9 +17,6 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 })
-
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']})
-
 
 const User = new mongoose.model('user', userSchema)
 
@@ -36,7 +33,7 @@ app.route('/login')
 
     .post(function(req, res){
         const username = req.body.username
-        const password = req.body.password
+        const password = md5(req.body.password)
 
         User.findOne({email: username}, function(err, foundUser){
             if(err) { 
@@ -60,7 +57,7 @@ app.route('/register')
     
 
     .post(function(req,res){
-        const newUser = new User({email: req.body.username, password: req.body.password})
+        const newUser = new User({email: req.body.username, password: md5(req.body.password)})
         
         newUser.save(function(err) {
             if(err) {
